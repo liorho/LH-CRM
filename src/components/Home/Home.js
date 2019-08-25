17e438ee594bad1dc19b2df8b3e4e411d3c46887
liorho <hoshealior@gmail.com>
2019-08-26 00:00:22 +0300

    finish design v2

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -15,11 +15,10 @@ class Home extends Component {
 
             <div className="home-container">
                 <ReactHowler
-                    // src='https://upload.wikimedia.org/wikipedia/commons/5/5d/Sunday_in_the_city_street_noise1.ogg'
                     src='https://upload.wikimedia.org/wikipedia/commons/e/e7/Cambodia_birds.ogg'
                     playing={true}
                 />
-                <div className="home"><span className="LH">LH</span></div>
+                <div className="LH">LH</div>
                 <div className="sit-back">Sit back. Relax. Let LH's CRM do all the hard work for you.</div>
             </div>
         )
