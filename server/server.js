91a5d69ebe8308e9a05c9cfad819d023fbfe25dd
Lior Hoshea <hoshealior@gmail.com>
2021-05-19 20:09:59 +0300

    changed app for production

--- server/server.js
+++ server/server.js
@@ -7,16 +7,23 @@ mongoose.connect('mongodb+srv://liorho:hoshea1234@cluster-1.fbmhg.mongodb.net/lh
   useFindAndModify: false,
 });
 
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-  next();
-});
+app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'node_modules')));
+
+// app.use(function (req, res, next) {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+//   next();
+// });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use('/', api);
+
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 const port = process.env.PORT || 4000;
 app.listen(port, function () {
